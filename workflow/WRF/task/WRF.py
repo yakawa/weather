6f0e91dacdd785fb07c1f0cab16008c6971edb9d
KAWASAKI Yasukazu <kawasaki@dev.kawa1128.jp>
2018-10-19 17:58:49 +0900

    WRF Preprocess

---
+++ workflow/WRF/task/WRF.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+import digdag
+
+import datetime
+import pathlib
+
+class WRFTools:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def get_init_time(cls, cur=None):
+        if cur is None:
+            now = datetime.datetime.utcnow()
+        else:
+            now = datetime.datetime.strptime(cur, '%Y%m%d%H%M')
+
+        now = now.replace(second=0, microsecond=0)
+        target = now - datetime.timedelta(hours=3, minutes=30)
+
+        if 0 <= target.hour < 6:
+            target = target.replace(hour=0, minute=0)
+        elif 6 <= target.hour < 12:
+            target = target.replace(hour=6, minute=0)
+        elif 12 <= target.hour < 18:
+            target = target.replace(hour=12, minute=0)
+        else:
+            target = target.replace(hour=18, minute=0)
+        return target
+
+
+class WRFPreProcess:
+    def __init__(self):
+        self.DATA_dir = pathlib.Path('/home/weather/outgoing/wrf')
+
+    def check_files(self):
+        init = WRFTools.get_init_time()
+        digdag.env.store({'init': init,})
+        for fn in self.DATA_dir.iterdir():
+            if not fn.name.stratswith(init.strftime('gfs.%Y%m&d_%H_')):
+                fn.unlink()
