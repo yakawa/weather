a77650d8cbc928c440f5cc3fc70477e65bb1a75a
KAWASAKI Yasukazu <kawasaki@dev.kawa1128.jp>
2018-10-26 08:58:49 +0900

    PostProcess

--- tools/processor/process_PostWRF.py
+++ tools/processor/process_PostWRF.py
@@ -9,12 +9,20 @@ import subprocess
 
 def main(args):
     fp = pathlib.Path(args.FILE)
+    if args.TERM == 'short':
+        p_name = 'postprocess_short'
+    elif args.TERM == 'week':
+        p_name = 'postprocess_week'
+    else:
+        return
+
     if fp.name == 'done':
-        r = subprocess.run(['/bin/sh', '-c', '/usr/local/bin/digdag start WRF post --session now'], check=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        r = subprocess.run(['/bin/sh', '-c', '/usr/local/bin/digdag start WRF {} --session now'.format(p_name)], check=True, stdin=subprocess.DEVNULL, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__DESC__)
     parser.add_argument('FILE', type=str, help='File Path')
+    parser.add_argument('TERM', type=str, help='Term')
     args = parser.parse_args()
 
     main(args)
